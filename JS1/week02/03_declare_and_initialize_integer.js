0c0a93b9f70d6fb875923c457684566efb6104c2
Yuliia Smirnova <julianna.smirnova@gmail.com>
2023-03-13 13:03:56 +0100

    js1/week02 - exercise 03 - add comments

--- JS1/week02/03_declare_and_initialize_integer.js
+++ JS1/week02/03_declare_and_initialize_integer.js
@@ -1,6 +1,17 @@
+// Initialize x
 let x;
+
+// Predicted value of x
 console.log("the value of x will be: undefined");
+
+//The actual value of x
 console.log(`the actual value of x is ${x}`);
+
+// Declare x
 x = 15;
+
+// Predicted value of x after declaring
 console.log("the value of x will be: 15");
-console.log(`the actual value of x is ${x}`);
+
+//The actual value of x
+console.log(x);

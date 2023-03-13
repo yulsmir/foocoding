78a28db72e831aa7e0505d950659d0898485517d
Yuliia Smirnova <julianna.smirnova@gmail.com>
2023-03-13 13:00:13 +0100

    js1/week02 - add exercise 03

---
+++ JS1/week02/03.js
@@ -0,0 +1,6 @@
+let x;
+console.log("the value of x will be: undefined");
+console.log(`the actual value of x is ${x}`);
+x = 15;
+console.log("the value of x will be: 15");
+console.log(`the actual value of x is ${x}`);

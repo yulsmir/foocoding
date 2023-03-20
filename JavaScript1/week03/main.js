d6d82b63054e6bada9c110f8aa7291b88121e9eb
Yuliia Smirnova <julianna.smirnova@gmail.com>
2023-03-20 14:31:41 +0100

    Add task05, fix task04

--- JavaScript1/week03/main.js
+++ JavaScript1/week03/main.js
@@ -20,17 +20,21 @@ favoriteAnimals.push(MAUROS_FAVORITE_ANIMAL);
 // 2.2 Log your new array!
 console.log(favoriteAnimals);
 
-// 2.3 Now add Jim's favorite animal to the array, it's 'meerkat', but make sure it will be placed after 'blowfish' and before 'capricorn'.
+// 2.3 Now add Jim's favorite animal to the array, it's 'meerkat', but make sure it will be
+// placed after 'blowfish' and before 'capricorn'.
 const JIMS_FAVORITE_ANIMAL = 'meerkat';
 favoriteAnimals.splice(1, 0, JIMS_FAVORITE_ANIMAL);
 
 // 2.4 Write a console.log statement that explains in words you think the new value of the array is.
-console.log(`The new value of array will be: ["blowfish", "meerkat", "capricorn", "giraffe"]`);
+console.log(
+  `The new value of array will be: ['blowfish', 'meerkat', 'capricorn', 'giraffe', 'turtle']`,
+);
 
 // 2.5 Log your new array!
 console.log(favoriteAnimals);
 
-// 2.6 Log the length of the array, add a message: 'The array has a length of: ' (here you should show the length of the array).
+// 2.6 Log the length of the array, add a message: 'The array has a length of:
+// ' (here you should show the length of the array).
 console.log('The array has a length of: ' + favoriteAnimals.length);
 
 // 2.7 Jason does not like 'giraffe', delete this animal from the array.
@@ -54,8 +58,9 @@ function sumThreeNumbers(num1, num2, num3) {
   return num1 + num2 + num3;
 }
 
-// TODO: to remove console.log
 console.log(sumThreeNumbers(4, 7, 10));
+console.log(sumThreeNumbers(567, 0, 18));
+console.log(sumThreeNumbers(9, 3, -20));
 
 // 2. Create a function named colorCar that receives a color, and prints out, 'a red car' for example.
 function colorCar(color) {
@@ -63,10 +68,12 @@ function colorCar(color) {
 }
 
 // TODO: to remove console.log
-console.log(colorCar('blue'));
+colorCar('blue');
+colorCar('black');
+colorCar('green');
 
 // 3. Create an object and a function that takes the object as a parameter and prints out all of its properties and values.
-const user = {
+const USER = {
   id: 1,
   name: 'admin',
   email: 'admin@email.com',
@@ -79,39 +86,89 @@ function printObjectPropertiesAndValue(object) {
   }
 }
 
-// TODO: to remove console.log
-printObjectPropertiesAndValue(user);
+printObjectPropertiesAndValue(USER);
 
 // 4. Create a function named vehicleType that receives a color, and a code, 1 for car, 2 for motorbike.
 // And prints 'a blue motorbike' for example when called as vehicleType("blue", 2)
 
 function vehicleType(color, code) {
   const VEHICLE_TYPES = ['car', 'motorbike'];
-
-  if (code === 1) {
-    return console.log(`a ${color} ${VEHICLE_TYPES[0]}`);
-  } else if (code === 2) {
-    return console.log(`a ${color} ${VEHICLE_TYPES[0]}`);
+  if (typeof color !== 'string' || typeof code !== 'number') {
+    console.log('Invalid input');
   } else {
-    return console.log('vehicle type is not defined')
+    if (code > VEHICLE_TYPES.length || code < 1) {
+      console.log(`Unknown vehicle type. Enter number from 1 to ${VEHICLE_TYPES.length}`);
+    } else {
+      if (code === 1) {
+        console.log(`a ${color} ${VEHICLE_TYPES[0]}`);
+      }
+      if (code === 2) {
+        console.log(`a ${color} ${VEHICLE_TYPES[1]}`);
+      }
+    }
   }
 }
 
-// TODO: to remove console.log
-vehicleType('yellow', 3);
-vehicleType('blue', 1);
-vehicleType('blue', 2);
-vehicleType('blue', -3);
+vehicleType('yellow', 1); // a yellow car
+vehicleType('blue', 0); // Unknown vehicle type. Enter number from 1 to 2
+vehicleType('white', 2); // a white motorbike
+vehicleType(true, NaN); // Invalid input
+vehicleType(1, 3); // Invalid input 
 
 // 5. Can you write the following without the if statement, but with just as a single line with console.log(...);?1
-// 6. Create a function called vehicle, like before, but takes another parameter called age, so that vehicle("blue", 1, 5) prints 'a blue used car'
-// 7. Make a list of vehicles, you can add "motorbike", "caravan", "bike", or more.
-// 8. How do you get the third element from that list?
-// 9. Change the function vehicle to use the list of question 7. So that vehicle("green", 3, 1) prints "a green new bike".
-// 10. Use the list of vehicles to write an advertisement. So that it prints something like: "Amazing Joe's Garage, we service cars, motorbikes, caravans and bikes.". (Hint: use a for loop.)
-// Hint, the output should be correct English with all the punctuation in place (that's the challenge). So plurals for the vehicle types, commas followed by a single space, the word and to replace the final comma and closed off by a period.
+// if (3 === 3) {
+//   console.log("yes");
+// } else {
+//   console.log("no");
+// }
+console.log(3 === 3 ? 'yes' : 'no');
 
-// 11. What if you add one more vehicle to the list, can you have that added to the advertisement without changing the code for question 10?
+// 6. Create a function called vehicle, like before, but takes another parameter called age,
+// so that vehicle("blue", 1, 5) prints 'a blue used car'
+// function vehicle(color, code, age) {
+//   const VEHICLE_TYPES = ['car', 'motorbike'];
+//   const CAR_CONDITIONS = ['unknown', 'new', 'used'];
+//   let carCondition = CAR_CONDITIONS[0];
+
+//   if (typeof color !== 'string' || typeof code !== 'number' || typeof age !== 'number') {
+//     console.log('Invalid input');
+//   } else {
+//     if (age >= 0 && age <= 1) {
+//       carCondition = CAR_CONDITIONS[1];
+//       if (code === 1) {
+//         return console.log(`a ${color} ${carCondition} ${VEHICLE_TYPES[0]}`);
+//       }
+
+//       if (code === 2) {
+//         return console.log(`a ${color} ${carCondition} ${VEHICLE_TYPES[1]}`);
+//       }
+//     } else if (age > 1) {
+//       carCondition = CAR_CONDITIONS[2];
+//       return console.log('boo');
+//     }
+//   }
+// }
+
+// console.log(vehicle('blue', 1, 5));
+// console.log(vehicle('white', 2, 1));
+// console.log(vehicle('green', 2, 0));
+
+// // 7. Make a list of vehicles, you can add "motorbike", "caravan", "bike", or more.
+// const VEHICLES = ['motorbike', 'caravan', 'bike', 'scooter'];
+
+// // 8. How do you get the third element from that list?
+// console.log(`Third element from list is: ` + VEHICLES[2]);
+
+// 9. Change the function vehicle to use the list of question 7.
+// So that vehicle("green", 3, 1) prints "a green new bike".
+// 10. Use the list of vehicles to write an advertisement. So that it prints something like:
+// "Amazing Joe's Garage, we service cars, motorbikes, caravans and bikes.". (Hint: use a for loop.)
+// Hint, the output should be correct English with all the punctuation in place (that's the challenge).
+// So plurals for the vehicle types, commas followed by a single space, the word and
+// to replace the final comma and closed off by a period.
+
+// 11. What if you add one more vehicle to the list, can you have that added to the advertisement
+// without changing the code for question 10 ?
 // 12. Create an empty object.
 // 13. Create an object that contains the teachers that you have had so far for the different modules.
 // 14. Add a property to the object you just created that contains the languages that they have taught you.
@@ -138,4 +195,5 @@ vehicleType('blue', -3);
 // let bar = 42;
 // typeof typeof bar;
 
-// ‘Coerce' means to try to change - so coercing var x = '6' to number means trying to change the type to number temporarily.
+// ‘Coerce' means to try to change - so coercing var x = '6' to number means
+// trying to change the type to number temporarily.

471e0fdb9c727ee3f01b2a24b7b44e6c26dcc96a
Yuliia Smirnova <julianna.smirnova@gmail.com>
2023-03-26 20:34:10 +0200

    Fix list displaying, add event listener to the document

--- JavaScript2/week01/app.js
+++ JavaScript2/week01/app.js
@@ -1,21 +1,19 @@
 'use strict';
 
-{
-  // Variables
+document.addEventListener('DOMContentLoaded', function () {
   const bookTitles = [
-    'harry_potter_and_chamber_of_secrets',
+    'harry_potter_and_the_chamber_of_secrets',
     'the_little_prince',
     'nineteen_eighty_four',
     'thinner',
     'the_science_of_interstellar',
-    'game_of_thrones:_clash_of_kings',
-    'the_theory_of_everything:_the_origin_and_fate_of_the_universe',
+    'game_of_thrones',
+    'the_theory_of_everything',
     'fahrenheit_451',
     'don_quixote',
     'invisible_man',
   ];
 
-  // Methods
   const createList = () => {
     const list = document.createElement('ul');
     for (let i = 0; i < bookTitles.length; i++) {
@@ -32,6 +30,67 @@
     document.getElementById('books').appendChild(list);
   };
 
+  const booksInfo = {
+    harry_potter_and_the_chamber_of_secrets: {
+      title: 'Harry potter and the chamber of secrets',
+      language: 'English',
+      author: 'J. K. Rowling',
+    },
+
+    the_little_prince: {
+      title: 'The little prince',
+      language: 'English',
+      author: 'Antoine de Saint-Exupéry',
+    },
+
+    nineteen_eighty_four: {
+      title: 'Nineteen eighty four',
+      language: 'English',
+      author: 'George Orwell',
+    },
+
+    thinner: {
+      title: 'Thinner',
+      language: 'English',
+      author: 'Stephen King',
+    },
+
+    the_science_of_interstellar: {
+      title: 'The science of interstellar',
+      language: 'English',
+      author: 'Kip Thorne',
+    },
+
+    game_of_thrones: {
+      title: 'Game of thrones',
+      language: 'English',
+      author: 'George R. R. Martin',
+    },
+
+    the_theory_of_everything: {
+      title: 'The theory of everything',
+      language: 'English',
+      author: 'Stephen Hawking',
+    },
+
+    fahrenheit_451: {
+      title: 'Fahrenheit 451',
+      language: 'English',
+      author: 'Ray Bradbury',
+    },
+
+    don_quixote: {
+      title: 'Don quixote',
+      language: 'English',
+      author: 'Miguel de Cervantes',
+    },
+
+    invisible_man: {
+      title: 'Invisible man',
+      language: 'English',
+      author: 'Ralph Ellison',
+    },
+  };
+
   createList();
-  // Inits & Event Listeners
-}
+});

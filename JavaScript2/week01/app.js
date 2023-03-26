b455d527df05891281ff90404fd2629e1944cf6e
Yuliia Smirnova <julianna.smirnova@gmail.com>
2023-03-26 18:36:30 +0200

    Add images of books

--- JavaScript2/week01/app.js
+++ JavaScript2/week01/app.js
@@ -1,11 +1,25 @@
 'use strict';
 
+// Variables
 {
   const bookTitles = [
     // Replace with your own book titles
-    'harry_potter_chamber_secrets',
+    'harry_potter_and_chamber_of_secrets',
+    'the_little_prince',
+    'nineteen_eighty_four',
+    'thinner',
+    'the_science_of_interstellar',
+    'game_of_thrones:_clash_of_kings',
+    'the_theory_of_everything:_the_origin_and_fate_of_the_universe',
+    'fahrenheit_451',
+    'don_quixote',
+    'invisible_man',
   ];
 
   // Replace with your own code
   console.log(bookTitles);
 }
+
+// Methods
+
+// Inits & Event Listeners

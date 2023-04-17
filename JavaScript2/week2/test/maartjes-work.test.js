2cc2ba1e93c33f947bd99e270172377c574b0445
Yuliia Smirnova <julianna.smirnova@gmail.com>
2023-04-17 18:11:09 +0200

    Fix code

--- JavaScript2/week2/test/maartjes-work.test.js
+++
@@ -1,9 +0,0 @@
-const { maartjesTasks, maartjesHourlyRate, computeEarnings } = require(`../homework/maartjes-work`);
-
-describe('maartjes_work', () => {
-  test('earnings rounded to euro cents', () => {
-    const earnings = computeEarnings(maartjesTasks, maartjesHourlyRate);
-    const result = earnings.toFixed(2);
-    expect(result).toBe('373.33');
-  });
-});
--- JavaScript2/week2/test/map-filter.test.js
+++
@@ -1,8 +0,0 @@
-const { myNumbers, doubleOddNumbers } = require(`../homework/map-filter`);
-
-describe('map_filter', () => {
-  test('result -> [2, 6]', () => {
-    const result = doubleOddNumbers(myNumbers);
-    expect(result).toEqual([2, 6]);
-  });
-});

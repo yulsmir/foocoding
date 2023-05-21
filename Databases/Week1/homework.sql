e22f312a8666958d59ad2874f52e08d9fed272bd
Yuliia Smirnova <julianna.smirnova@gmail.com>
2023-05-21 23:45:05 +0200

    Insert values into locations

--- Databases/Week1/homework.sql
+++ Databases/Week1/homework.sql
@@ -8,7 +8,7 @@ create database HR;
 -- 2. Create table employee;
 use HR;
 begin;
-create table employee(
+create table employees(
   employee_id int not null auto_increment,
   first_name varchar(255) not null,
   last_name varchar(255) not null,
@@ -16,11 +16,9 @@ create table employee(
   title varchar(255) not null,
   salary int not null,
   department varchar(255) not null,
-  location varchar(255),
   start_date date not null,
   -- constraints
-  primary key(employee_id),
-  unique key(employee_id)
+  primary key(employee_id)
 );
 
 mysql> show tables;
@@ -53,18 +51,16 @@ create table locations(
   country varchar(255),
   city varchar(255),
   street varchar(255),
-  address varchar(255),
   postal_code varchar(255),
   employee_id int,
   -- constraints
   primary key(location_id),
   foreign key(employee_id)
-    references employee(employee_id),
-  unique(location_id, employee_id)
+    references employees(employee_id)
 );
 
 -- 4. Insert 10-20 rows in each table with relevant fields. (Make sure that you have relevant relations)
-insert into employee (first_name, last_name, title, salary, department, start_date) 
+insert into employees (first_name, last_name, title, salary, department, start_date) 
 values 
   ('Tom', 'Tomsson', 'Sales Manager', 320000, 'Sales', '1999-12-31'), 
   ('John', 'Johnsson', 'PR Manager', 400000, 'PR', '2020-12-25'), 
@@ -96,3 +92,17 @@ mysql> select * from employee;
 |          11 | Stewie     | Nobody Knows | Son               | 333333 | Family Guy | NULL     | 2000-02-03 |
 +-------------+------------+--------------+-------------------+--------+------------+----------+------------+
 11 rows in set (0.01 sec)
+
+insert into locations(country, city, street, postal_code, employee_id)
+values
+  ('Poland', 'Krakow', 'Wlodecka 16', '23456', 1),
+  ('Sweden', 'Stockholm', 'Vasagatan 15', '63567', 2),
+  ('Sweden', 'Gothenburg', 'Kristiansgatan 2', '43555', 3),
+  ('Sweden', 'Ystad', 'Lillatorget 1', '33344', 4),
+  ('Sweden', 'Kiruna', 'Storgatan 6', '66554', 5),
+  ('Wonderland', 'Wondercity', 'Wonder street 888', '88888', 6),
+  ('USA', 'Springfield', 'Unknown Street 13', '77777', 8),
+  ('USA', 'Springfield', 'Unknown Street 13', '77777', 9),
+  ('USA', 'Springfield', 'Unknown Street 13', '77777', 10),
+  ('USA', 'Quahog', 'Strange street 9', '88888', 11)
+;

4b590c2981719627a2e6d5a7c4f51893dc456690
Yuliia Smirnova <julianna.smirnova@gmail.com>
2023-05-22 14:22:33 +0200

    Add answers to questions 4, 5

--- Databases/Week1/answer_questions.sql
+++ Databases/Week1/answer_questions.sql
@@ -1,15 +1,19 @@
 
 -- 1. What are the names of countries with population greater than 8 million
-select country.name from country where population > 8000000;
+select country.name, population from country where population > 8000000;
 
 -- 2. What are the names of countries that have “land” in their names ?
 select country.name from country where country.name like '%land%';
 
 -- 3. What are the names of the cities with population in between 500,000 and 1 million ?
-select country.name from country where population between 500000 and 1000000;
+select country.name, population from country where population between 500000 and 1000000;
 
--- What's the name of all the countries on the continent ‘Europe’ ?
--- List all the countries in the descending order of their surface areas.
+-- 4. What's the name of all the countries on the continent ‘Europe’ ?
+select country.name, continent from country where continent = 'Europe';
+
+-- 5. List all the countries in the descending order of their surface areas.
+select country.name, surfacearea from country order by surfacearea desc;
+
 -- What are the names of all the cities in the Netherlands?
 -- What is the population of Rotterdam ?
 -- What's the top 10 countries by Surface Area ?

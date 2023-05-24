8dfd159e0f8acc56c4d07476c6590254ed3ff960
Yuliia Smirnova <julianna.smirnova@gmail.com>
2023-05-24 15:31:59 +0200

    Add extra task to databases homework

---
+++ Databases/Week1/03_extra_task.sql
@@ -0,0 +1,24 @@
+-- Extra task:
+-- 1. What are the names of the countries that has more than 10 cities, and a total 
+-- population of the cities listed (not the country population) of more than 50 million?
+select country.name, sum(city.population) as total_city_population
+from country
+join city on country.Code = city.countrycode
+group by country.name
+having count(city.name) > 10 and sum(city.population) > 50000000
+order by total_city_population ASC;
+
+
+-- 2. List the cities from those countries, where the city population is > 5M
+select city.name, city.population
+from country
+join city on country.code = city.countrycode
+where country.name in (
+  select country.name
+  from country
+  join city on country.code = city.countrycode
+  group by country.name
+  having count(city.name) > 10 and sum(city.population) > 50000000
+)
+and city.population > 5000000
+order by city.population ASC;

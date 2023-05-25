32cd32c34610f59f1736958639b9184a043b4370
Yuliia Smirnova <julianna.smirnova@gmail.com>
2023-05-25 12:24:41 +0200

    Fix extra task: join => inner join, remove join in main query(task2)

--- Databases/Week1/03_extra_task.sql
+++ Databases/Week1/03_extra_task.sql
@@ -3,21 +3,20 @@
 -- population of the cities listed (not the country population) of more than 50 million?
 select country.name, sum(city.population) as total_city_population
 from country
-join city on country.code = city.countrycode
+inner join city on country.code = city.countrycode
 group by country.name
 having count(city.name) > 10 and sum(city.population) > 50000000
 order by total_city_population ASC;
 
 -- 2. List the cities from those countries, where the city population is > 5M
 select city.name, city.population
-from country
-join city on country.code = city.countrycode
-where country.name in (
-  select country.name
+from city
+where countrycode in (
+  select country.code
   from country
-  join city on country.code = city.countrycode
-  group by country.name
+  inner join city on country.code = city.countrycode
+  group by country.code
   having count(city.name) > 10 and sum(city.population) > 50000000
 )
 and city.population > 5000000
-order by city.population ASC;
+order by population ASC;

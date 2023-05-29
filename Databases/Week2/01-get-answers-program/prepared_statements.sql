c17e8a300b57c469038a844496265f313bbcfc36
Yuliia Smirnova <julianna.smirnova@gmail.com>
2023-05-29 22:51:00 +0200

    Add region_language statement

--- Databases/Week2/01-get-answers-program/prepared_statements.sql
+++
@@ -1,9 +0,0 @@
--- 1. What is the capital of country X ? (Accept X from user)
--- 2. List all the languages spoken in the region Y (Accept Y from user) 
--- 3. Find the number of cities in which language Z is spoken (Accept Z from user)
--- 4. List all the continents with the number of languages spoken in each continent
--- 5. For the country given as input, is there any countries that
-  -- have the same official language
-  -- is in the same continent
--- If yes, display those countries.
--- If no, display FALSE

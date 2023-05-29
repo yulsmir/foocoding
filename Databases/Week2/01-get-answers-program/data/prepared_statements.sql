e64aee4f6399a4cebd64fda9695f44b603bf71a8
Yuliia Smirnova <julianna.smirnova@gmail.com>
2023-05-29 23:53:18 +0200

    Add list_all_continents_with_languages_count prepared statement

--- Databases/Week2/01-get-answers-program/data/prepared_statements.sql
+++ Databases/Week2/01-get-answers-program/data/prepared_statements.sql
@@ -4,15 +4,14 @@ prepare show_country_capital from 'select country.name , city.name from city inn
 
 -- TODO: remove value Caribbean in the query and put '?'
 -- 2. List all the languages spoken in the region Y (Accept Y from user) 
-prepare list_all_region_laguages from 'select language from countrylanguage inner join country on country.code = countrylanguage.countrycode where country.region = "Caribbean" group by language;';
+prepare list_all_languages_in_region from 'select language from countrylanguage inner join country on country.code = countrylanguage.countrycode where country.region = "Caribbean" group by language;';
 
 -- TODO: remove value Slovak in the query and put '?'
 -- 3. Find the number of cities in which language Z is spoken (Accept Z from user)
 prepare count_cities_where_language_is_spoken from 'select count(1) from city inner join countrylanguage on city.countrycode = countrylanguage.countrycode where countrylanguage.language = "Slovak";';
 
--- TODO: remove value in the query and put '?'
 -- 4. List all the continents with the number of languages spoken in each continent
-prepare 
+prepare list_all_continents_with_languages_count from 'select country.continent, count(countrylanguage.language) as languages_number from country inner join countrylanguage on country.code = countrylanguage.countrycode group by country.continent;';
 
 -- TODO: remove value in the query and put '?'
 -- 5. For the country given as input, is there any countries that

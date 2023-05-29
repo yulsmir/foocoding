3b667a0959206e2319d9c5c54d3ca1ed4bea5c6b
Yuliia Smirnova <julianna.smirnova@gmail.com>
2023-05-30 01:50:30 +0200

    Create prepared_statements.js module

--- Databases/Week2/01-get-answers-program/index.js
+++ Databases/Week2/01-get-answers-program/index.js
@@ -1,5 +1,13 @@
+'use strict';
+
 const mysql = require('mysql2');
 const readline = require('readline');
+const {
+  showCountryCapital,
+  listAllLanguagesInRegion,
+  countCitiesWhereLanguageIsSpoken,
+  listAllContinentsWithLanguagesCount,
+} = require('./prepared_statements');
 
 const connection = mysql.createConnection({
   host: 'localhost',
@@ -27,6 +35,18 @@ connection.connect(function (err) {
 // Main code
 const main = () => {
   console.log('Do some magic');
+  connection.query(
+    'SELECT country.name, city.name FROM city INNER JOIN country ON capital = city.id WHERE country.name = "Germany";',
+    function (err, results, fields) {
+      if (err) {
+        console.error('Error executing query:', err);
+        connection.end(); // Close the connection in case of an error
+        return;
+      }
+      console.log(results); // Log the results inside the callback
+      connection.end(); // Close the connection after the query is completed
+    },
+  );
 };
 
 const getUserInputFromConsole = (question) => {
@@ -36,11 +56,3 @@ const getUserInputFromConsole = (question) => {
     });
   });
 };
-
-// Finish connection in the end
-connection.end(function (err) {
-  if (err) {
-    return console.log('error:' + err.message);
-  }
-  console.log('Close the database connection.');
-});
---
+++ Databases/Week2/01-get-answers-program/prepared_statements.js
@@ -0,0 +1,35 @@
+'use strict';
+
+// -- 1. What is the capital of country X ? (Accept X from user)
+// -- TODO: remove value Germany in the query and put '?'
+const showCountryCapital =
+  'select country.name , city.name from city inner join country on capital = city.id where country.name = "Germany";';
+
+// -- TODO: remove value Caribbean in the query and put '?'
+// -- 2. List all the languages spoken in the region Y (Accept Y from user)
+const listAllLanguagesInRegion =
+  'select language from countrylanguage inner join country on country.code = countrylanguage.countrycode where country.region = "Caribbean" group by language;';
+
+// -- TODO: remove value Slovak in the query and put '?'
+// -- 3. Find the number of cities in which language Z is spoken (Accept Z from user)
+const countCitiesWhereLanguageIsSpoken =
+  'select count(1) from city inner join countrylanguage on city.countrycode = countrylanguage.countrycode where countrylanguage.language = "Slovak";';
+
+// -- 4. List all the continents with the number of languages spoken in each continent
+const listAllContinentsWithLanguagesCount =
+  'select country.continent, count(countrylanguage.language) as languages_number from country inner join countrylanguage on country.code = countrylanguage.countrycode group by country.continent;';
+
+// -- TODO: remove value in the query and put '?'
+// -- 5. For the country given as input, is there any countries that
+// -- have the same official language
+// -- is in the same continent
+
+// -- If yes, display those countries.
+// -- If no, display FALSE
+
+module.exports = {
+  showCountryCapital,
+  listAllLanguagesInRegion,
+  countCitiesWhereLanguageIsSpoken,
+  listAllContinentsWithLanguagesCount,
+};

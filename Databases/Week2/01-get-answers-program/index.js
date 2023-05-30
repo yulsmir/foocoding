85790b3722b53273459d8c38eb6f9ec0168524f4
Yuliia Smirnova <julianna.smirnova@gmail.com>
2023-05-30 02:04:54 +0200

    Add listAllLanguagesInRegion function

--- Databases/Week2/01-get-answers-program/index.js
+++ Databases/Week2/01-get-answers-program/index.js
@@ -45,7 +45,17 @@ const showCountryCapital = () => {
     connection.end(); // Close the connection after the query is completed
   });
 };
-const listAllLanguagesInRegion = () => {};
+const listAllLanguagesInRegion = () => {
+  connection.query(allLanguagesInRegionList, function (err, results, fields) {
+    if (err) {
+      console.error('Error executing query:', err);
+      connection.end(); // Close the connection in case of an error
+      return;
+    }
+    console.log(results); // Log the results inside the callback
+    connection.end(); // Close the connection after the query is completed
+  });
+};
 const countCitiesWhereLanguageIsSpoken = () => {};
 const listAllContinentsWithLanguagesCount = () => {};
 
@@ -61,4 +71,5 @@ const getUserInputFromConsole = (question) => {
 const main = () => {
   console.log('Do some magic');
   showCountryCapital();
+  listAllLanguagesInRegion();
 };

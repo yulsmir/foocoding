78ff54885b786c68a172d7c579ebf444a1690687
Yuliia Smirnova <julianna.smirnova@gmail.com>
2023-06-01 20:11:22 +0200

    Fix schema

--- Databases/Week2/02-todo-app/todo_app.sql
+++ Databases/Week2/02-todo-app/todo_app.sql
@@ -17,12 +17,20 @@ create table if not exists todoitem (
   name varchar(255),
   list_id int,
   completed boolean,
-  reminder datetime,
   -- constraints
   primary key (id),
   foreign key (list_id) references todolist (id)
 );
 
+-- Create table reminder
+create table if not exists reminder (
+  id int auto_increment not null,
+  remind_at datetime,
+  item_id int,
+  -- constraints
+  primary key (id),
+  foreign key (item_id) references todoitem(id)
+);
 -- Create table tag
 create table if not exists tag(
   id int auto_increment,
@@ -40,21 +48,32 @@ create table if not exists tagsitems (
   foreign key (tag_id) references tag(id)
 );
 
+
+
 -- Inserting some data into todolist
 insert into todolist (name) values
   ('Some stuff todo'),
   ('Wash Car'),
   ('Homework'),
-  ('Grocery List');
+  ('Grocery List')
 ;
 
 -- Insert some data into todoitem
-insert into todoitem (name, list_id, completed, reminder) values
-  ('Buy bread', 3, 0, '2024-12-05 11:00:00'),
-  ('Finish something', 2, 0, '2024-12-05 11:00:00'),
-  ('Go for a coffee', 1, 1, null),
-  ('But sneackers', 2, 1,'2024-12-05 11:00:00')
+insert into todoitem (name, list_id, completed) values
+  ('Buy bread', 3, 0),
+  ('Finish something', 2, 0),
+  ('Go for a coffee', 1, 1),
+  ('But sneackers', 2, 1)
 ;
+
+-- Insert some data into reminder table
+insert into reminder (remind_at, item_id) values
+  ('2024-11-05 11:00:00', 1),
+  ('2024-12-05 11:00:00', 1),
+  ('2024-11-05 11:00:00', 2),
+  ('2024-11-05 11:00:00', 4)
+;
+
 -- Insert some data into tag table
 insert into tag (name) values
   ('Stuff'),
@@ -77,3 +96,4 @@ select * from todoitem;
 select * from todolist;
 select * from user;
 select * from tagsitems;
+select * from reminder;

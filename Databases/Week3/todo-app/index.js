dbccc598e7c32fafc73b9c9e76764a665f331069
Yuliia Smirnova <julianna.smirnova@gmail.com>
2023-06-05 16:52:29 +0200

    Add dotenv package, add main function

--- Databases/Week3/todo-app/index.js
+++ Databases/Week3/todo-app/index.js
@@ -1,68 +1,102 @@
 'use strict';
+// Password secure usage
+require('dotenv').config();
 
 const express = require('express');
 const mysql = require('mysql2');
+const port = 3000;
+const app = express();
+
+// Define routes
+const router = express.Router();
 
 // Create a MySQL connection pool
-const pool = mysql.createPool({
+const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: process.env.DB_PASSWORD,
   database: 'todo_app',
 });
 
-// Define routes
-const router = express.Router();
+const executeQuery = (userRequest, mySqlRequest, callback) => {
+  const request = prompt(userRequest);
 
-// FUNCTIONS
-const showList = (req, res) => {
-  console.log('list shown');
+  connection.prepare(mySqlRequest, (err, statement) => {
+    console.log(statement);
+    statement.execute([request], callback);
+    statement.close();
+  });
 };
 
-const createList = (req, res) => {
-  console.log('List is created');
+const handleError = (err, results) => {
+  if (results) {
+    console.log(results);
+  } else {
+    console.log('An error occurred:', err);
+  }
 };
 
-const deleteList = (req, res) => {
-  console.log('List is deleted');
+const main = async () => {
+  // FUNCTIONS
+  // const showList = (req, res) => {};
+
+  // const createList = (req, res) => {
+  //   console.log('List is created');
+  // };
+
+  // const deleteList = (req, res) => {
+  //   console.log('List is deleted');
+  // };
+
+  // const addListReminder = (req, res) => {
+  //   console.log('Reminder is added');
+  // };
+
+  // const addItem = (req, res) => {
+  //   console.log('Item is added to a list');
+  // };
+
+  const userId = 1;
+  const listId = 1;
+
+  // ---- LISTS ----
+  // Get user's todo list
+  router.get(`/:user${userId}/lists/${listId}`, (req, res) => {
+    console.log('list shown');
+    res.status(200).json({ result: 'list' });
+  });
+
+  // Create a todo list
+  router.post(`/:user${userId}/lists`, (req, res) => {
+    console.log('List is created');
+    res.status(200).json({ result: 'lists with new list' });
+  });
+
+  // Delete a todo list
+  router.delete(`/:user${userId}/lists/:${listId}`, (req, res) => {
+    console.log('List is deleted');
+    res.status(201).json({ result: 'list' });
+  });
+
+  // Add reminder for the list
+  router.post(`/:user${userId}/lists/:${listId}/reminders`, (req, res) => {
+    console.log('Reminder is added');
+  });
+
+  // --- ITEMS ----
+  // Insert item(s) in todo list
+  router.patch(`/:user${userId}/lists/:${listId}/items`, (req, res) => {
+    console.log('Item is added to the list');
+  });
+
+  // Middleware
+  app.use(express.json());
+  app.use('/', router);
+
+  // Start the server
+  app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+  });
 };
 
-const addListReminder = (req, res) => {
-  console.log('Reminder is added');
-};
-
-const addItem = (req, res) => {
-  console.log('Item is added to a list');
-};
-
-const userId = 1;
-const listId = 1;
-
-// ---- LISTS ----
-// Get user's todo list
-router.get(`/:user${userId}/lists/${listId}`, showList);
-
-// Create a todo list
-router.post(`/:user${userId}/lists`, createList);
-
-// Delete a todo list
-router.delete(`/:user${userId}/lists/:${listId}`, deleteList);
-
-// Add reminder for the list
-router.post(`/:user${userId}/lists/:${listId}/reminders`, addListReminder);
-
-// --- ITEMS ----
-// Insert item(s) in todo list
-router.post(`/:user${userId}/lists/:${listId}/items`, addItem);
-
-const app = express();
-const port = 3000;
-
-// Middleware
-app.use(express.json());
-app.use('/', router);
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+main();

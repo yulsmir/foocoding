ff30e6e4d556ba8202f4f7fea78518ed8eed7d55
Yuliia Smirnova <julianna.smirnova@gmail.com>
2023-06-05 03:58:03 +0200

    Fix sql dump

--- Databases/Week3/todo-app/db.sql
+++ Databases/Week3/todo-app/db.sql
@@ -3,12 +3,32 @@ create database if not exists todo_app;
 
 use todo_app;
 
--- Create table todolist
-create table if not exists todolist (
+-- Create table user
+create table if not exists user (
   id int auto_increment not null,
   name varchar(255),
   -- constraints 
   primary key (id)
+);
+
+-- Create table todolist
+create table if not exists todolist (
+  id int auto_increment not null,
+  name varchar(255),
+  user_id int,
+  -- constraints 
+  primary key (id),
+  foreign key (user_id) references user(id)
+);
+
+-- Create table reminder for a list
+create table if not exists listreminders (
+  id int auto_increment not null,
+  remind_at datetime,
+  list_id int,
+  -- constraints
+  primary key (id),
+  foreign key (list_id) references todolist(id)
 );
 
 -- Create table todoitem
@@ -19,11 +39,11 @@ create table if not exists todoitem (
   completed boolean,
   -- constraints
   primary key (id),
-  foreign key (list_id) references todolist (id)
+  foreign key (list_id) references todolist(id)
 );
 
 -- Create table reminder
-create table if not exists reminder (
+create table if not exists itemreminders (
   id int auto_increment not null,
   remind_at datetime,
   item_id int,
@@ -31,6 +51,7 @@ create table if not exists reminder (
   primary key (id),
   foreign key (item_id) references todoitem(id)
 );
+
 -- Create table tag
 create table if not exists tag(
   id int auto_increment,
@@ -48,29 +69,39 @@ create table if not exists tagsitems (
   foreign key (tag_id) references tag(id)
 );
 
+-- Insert some data into user
+insert into user (name) values 
+  ('user1'),
+  ('user2')
+;
 
 -- Inserting some data into todolist
-insert into todolist (name) values
-  ('Some stuff todo'),
-  ('Wash Car'),
-  ('Homework'),
-  ('Grocery List')
+insert into todolist (name, user_id) values
+  ('Some stuff todo', 1),
+  ('Homework', 2)
 ;
 
 -- Insert some data into todoitem
 insert into todoitem (name, list_id, completed) values
-  ('Buy bread', 3, 0),
+  ('Buy bread', 1, 0),
   ('Finish something', 2, 0),
   ('Go for a coffee', 1, 1),
   ('But sneackers', 2, 1)
 ;
 
--- Insert some data into reminder table
-insert into reminder (remind_at, item_id) values
+-- Insert some data into itemreminders table
+insert into itemreminders (remind_at, item_id) values
   ('2024-11-05 11:00:00', 1),
   ('2024-12-05 11:00:00', 1),
   ('2024-11-05 11:00:00', 2),
   ('2024-11-05 11:00:00', 4)
+;
+
+-- Insert some data into listreminders table
+insert into listreminders (remind_at, list_id) values
+  ('2024-11-05 11:00:00', 1),
+  ('2024-11-05 11:00:00', 2)
+
 ;
 
 -- Insert some data into tag table
@@ -86,12 +117,14 @@ insert into tagsitems (item_id, tag_id) values
   (3, 1),
   (4, 1),
   (4, 2),
-  (1, 1);
+  (1, 1)
 ;
 -- View data
 show tables;
+select * from user;
 select * from tag;
 select * from todoitem;
 select * from todolist;
 select * from tagsitems;
-select * from reminder;
+select * from itemreminders;
+select * from listreminders;

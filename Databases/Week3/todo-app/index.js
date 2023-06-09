1cbe04c69f2f885fb45e0ab4ba55de03493153e6
Yuliia Smirnova <julianna.smirnova@gmail.com>
2023-06-09 23:28:38 +0200

    Fix host

--- Databases/Week3/todo-app/index.js
+++ Databases/Week3/todo-app/index.js
@@ -10,7 +10,7 @@ const mysql = require('mysql2');
 
 // Create a MySQL connection pool
 const connection = mysql.createConnection({
-  host: process.env.MYSQL_HOST,
+  host: '127.0.0.1',
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: 'todo_app',

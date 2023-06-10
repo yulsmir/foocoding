10ec3d55797ac941b9ec502c1b54cc5d00218c90
Yuliia Smirnova <julianna.smirnova@gmail.com>
2023-06-10 14:30:04 +0200

    Return errors handler

--- Databases/Week3/todo-app/index.js
+++ Databases/Week3/todo-app/index.js
@@ -2,6 +2,7 @@
 
 // Password secure usage
 require('dotenv').config();
+// const { readFileSync } = require('fs');
 
 const express = require('express');
 const port = 3000;
@@ -19,137 +20,185 @@ const connection = mysql.createConnection({
   rowsAsArray: true,
 });
 
-const main = async () => {
-  let userId;
-  let listId;
-  let itemId;
+// // Setup databse from sql dump file
+// const getSqlDump = (path) => {
+//   return readFileSync(path, 'utf-8');
+// };
 
-  // ---- LISTS ----
+// const preparedDb = getSqlDump('./todo_app.sql');
 
-  // Homepage
-  app.get('/', (req, res) => {
-    res.send('<h1>todoapp</h1>');
-  });
+// // Prepare db
+// connection.query(preparedDb, (err, res) => {
+//   if (err) {
+//     console.error(err);
+//   }
+//   console.log('Database set up completed');
+// });
 
-  // Get user's todo lists
-  app.get(`/:userId/lists`, (req, res) => {
-    userId = req.params.userId;
+// Main app
+// const main = async () => {
+let userId;
+let listId;
+let itemId;
 
-    const sql = 'select id, name from todolist where user_id = ?';
+// ---- LISTS ----
 
-    connection.query(sql, [userId], (err, results) => {
+// Homepage
+app.get('/', (req, res) => {
+  res.send('<h1>todoapp</h1>');
+});
+
+// Get user's todo lists
+app.get(`/:userId/lists`, (req, res) => {
+  userId = req.params.userId;
+
+  const sql = 'select id, name from todolist where user_id = ?';
+
+  connection.query(sql, [userId], (err, results) => {
+    if (err) {
+      res.status(404).json({ error: err });
+    } else {
       res.status(200).json({ userId: userId, lists: results });
-    });
+    }
   });
+});
 
-  // Get user's todo list by id
-  app.get(`/:userId/lists/:listId`, (req, res) => {
-    userId = req.params.userId;
-    listId = req.params.listId;
+// Get user's todo list by id
+app.get(`/:userId/lists/:listId`, (req, res) => {
+  userId = req.params.userId;
+  listId = req.params.listId;
 
-    const sql = 'select id, name from todolist where user_id = ? and id = ?';
+  const sql = 'select id, name from todolist where user_id = ? and id = ?';
 
-    connection.query(sql, [userId, listId], (err, results) => {
+  connection.query(sql, [userId, listId], (err, results) => {
+    if (err) {
+      res.status(404).json({ error: err });
+    } else {
       res.status(200).json({ userId: userId, listId: listId, results });
-    });
+    }
   });
+});
 
-  // Create a todo list
-  app.post(`/:userId/lists`, (req, res) => {
-    userId = req.params.userId;
+// Create a todo list
+app.post(`/:userId/lists`, (req, res) => {
+  userId = req.params.userId;
 
-    const sql = `insert into todolist (name, user_id) values('Newly created list to todo', ?)`;
+  const sql = `insert into todolist (name, user_id) values('Newly created list to todo', ?)`;
 
-    connection.query(sql, [userId], (err, results) => {
+  connection.query(sql, [userId], (err, results) => {
+    if (err) {
+      res.status(404).json({ error: err });
+    } else {
       res.status(200).json({ userId: userId, listId: listId, results });
-    });
+    }
   });
+});
 
-  // Delete a todo list by id
-  app.delete(`/:userId/lists/:listId`, (req, res) => {
-    userId = req.params.userId;
-    listId = req.params.listId;
+// Delete a todo list by id
+app.delete(`/:userId/lists/:listId`, (req, res) => {
+  userId = req.params.userId;
+  listId = req.params.listId;
 
-    const sql = `delete from todolist where user_id = ? and id = ?`;
+  const sql = `delete from todolist where user_id = ? and id = ?`;
 
-    connection.query(sql, [userId, listId], (err, results) => {
+  connection.query(sql, [userId, listId], (err, results) => {
+    if (err) {
+      res.status(404).json({ error: err });
+    } else {
       res.status(200).json({
         userId: userId,
         listId: listId,
         results: `List with id ${listId} is deleted successfully`,
       });
-    });
+    }
   });
+});
 
-  // Add reminder to the list
-  app.post(`/:userId/lists/:listId/reminders`, (req, res) => {
-    listId = req.params.listId;
+// Add reminder to the list
+app.post(`/:userId/lists/:listId/reminders`, (req, res) => {
+  listId = req.params.listId;
 
-    const sql = `insert into listreminders (remind_at, list_id) values('2024-11-12 22:10:00', ?)`;
+  const sql = `insert into listreminders (remind_at, list_id) values('2024-11-12 22:10:00', ?)`;
 
-    connection.query(sql, [listId], (err, results) => {
+  connection.query(sql, [listId], (err, results) => {
+    if (err) {
+      res.status(404).json({ error: err });
+    } else {
       res.status(200).json({
         listId: listId,
         results: `Reminder for list ${listId} is added successfully`,
       });
-    });
+    }
   });
+});
 
-  // ---- ITEMS ----
-  // Insert item(s) in todo list
-  app.post(`/:userId/lists/:listId/items`, (req, res) => {
-    userId = req.params.userId;
-    listId = req.params.listId;
+// ---- ITEMS ----
+// Insert item(s) in todo list
+app.post(`/:userId/lists/:listId/items`, (req, res) => {
+  userId = req.params.userId;
+  listId = req.params.listId;
 
-    const sql = `insert into todoitem (name, list_id, completed) values('Buy something new', ?, 0)`;
+  const sql = `insert into todoitem (name, list_id, completed) values('Buy something new', ?, 0)`;
 
-    connection.query(sql, [listId], (err, results) => {
+  connection.query(sql, [listId], (err, results) => {
+    if (err) {
+      res.status(404).json({ error: err });
+    } else {
       res.status(200).json({
         listId: listId,
         results: `Todoitem into list ${listId} is added successfully`,
       });
-    });
+    }
   });
+});
 
-  // Delete item(s) from todo list
-  app.delete(`/:userId/lists/:listId/items/:itemId`, (req, res) => {
-    itemId = req.params.itemId;
-    listId = req.params.listId;
+// Delete item(s) from todo list
+app.delete(`/:userId/lists/:listId/items/:itemId`, (req, res) => {
+  itemId = req.params.itemId;
+  listId = req.params.listId;
 
-    const sql = `delete from todoitem where id = ? and list_id = ?`;
+  const sql = `delete from todoitem where id = ? and list_id = ?`;
 
-    connection.query(sql, [itemId, listId], (err, results) => {
+  connection.query(sql, [itemId, listId], (err, results) => {
+    if (err) {
+      res.status(404).json({ error: err });
+    } else {
       res.status(200).json({
         listId: listId,
         results: `Item with id ${itemId} is deleted successfully`,
       });
-    });
+    }
   });
+});
 
-  // Mark item as completed
-  app.patch(`/:userId/lists/:listId/items/:itemId`, (req, res) => {
-    listId = req.params.listId;
-    itemId = req.params.itemId;
+// Mark item as completed
+app.patch(`/:userId/lists/:listId/items/:itemId`, (req, res) => {
+  listId = req.params.listId;
+  itemId = req.params.itemId;
 
-    const sql = `update todoitem set completed = true where id = ? and list_id = ?`;
-    connection.query(sql, [itemId, listId], (err, results) => {
+  const sql = `update todoitem set completed = true where id = ? and list_id = ?`;
+  connection.query(sql, [itemId, listId], (err, results) => {
+    if (err) {
+      res.status(404).json({ error: err });
+    } else {
       res.status(200).json({
         listId: listId,
         results: `Item with id ${itemId} is marked completed`,
       });
-    });
+    }
   });
+});
 
-  // Middleware
-  app.use(express.json());
-  app.use((err, req, res, next) => {
-    res.status(500).json('Some error');
-  });
+// Middleware
+app.use(express.json());
+app.use((err, req, res, next) => {
+  res.status(500).json('Some error');
+});
 
-  // Start the server
-  app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-  });
-};
+// Start the server
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
+// };
 
-main();
+// main();

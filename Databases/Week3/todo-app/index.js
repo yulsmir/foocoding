bb68b31cd5bc74f6883dba446c58a20d6a350dcd
Yuliia Smirnova <julianna.smirnova@gmail.com>
2023-06-11 18:13:56 +0200

    Rewrite functions with mysql2/promises, use prepare statement

--- Databases/Week3/todo-app/index.js
+++ Databases/Week3/todo-app/index.js
@@ -1,21 +1,55 @@
 'use strict';
 
-// Password secure usage
 require('dotenv').config();
-
 const express = require('express');
 const port = 3000;
 const app = express();
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 
 // Create a MySQL connection pool
-const connection = mysql.createConnection({
+const pool = mysql.createPool({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
   rowsAsArray: true,
 });
+
+// Prepare the connection
+async function prepareConnection() {
+  const connection = await pool.getConnection();
+  return connection;
+}
+
+// Execute a prepared statement
+async function executeStatement(connection, statement, params) {
+  const preparedStmt = await connection.prepare(statement);
+  const [results] = await preparedStmt.execute(params);
+  return results;
+}
+
+// Unprepare the connection
+async function unprepareConnection(connection) {
+  connection.release();
+}
+
+// Handle errors
+function handleError(res, error) {
+  res.status(404).json({ error: error });
+}
+
+// Get data from the database
+async function getData(res, statement, params) {
+  const connection = await prepareConnection();
+  try {
+    const results = await executeStatement(connection, statement, params);
+    return results;
+  } catch (error) {
+    handleError(res, error);
+  } finally {
+    await unprepareConnection(connection);
+  }
+}
 
 // ---- LISTS ----
 // Homepage
@@ -24,143 +58,97 @@ app.get('/', (req, res) => {
 });
 
 // Get user's todo lists
-app.get(`/:userId/lists`, (req, res) => {
+app.get('/:userId/lists', async (req, res) => {
   const userId = req.params.userId;
+  const statement = 'SELECT id, name FROM todolist WHERE user_id = ?';
 
-  const sql = 'select id, name from todolist where user_id = ?';
-
-  connection.query(sql, [userId], (err, results) => {
-    if (err) {
-      res.status(404).json({ error: err });
-    } else {
-      res.status(200).json({ userId: userId, lists: results });
-    }
-  });
+  const results = await getData(res, statement, [userId]);
+  res.status(200).json({ userId: userId, lists: results });
 });
 
 // Get user's todo list by id
-app.get(`/:userId/lists/:listId`, (req, res) => {
+app.get('/:userId/lists/:listId', async (req, res) => {
   const userId = req.params.userId;
   const listId = req.params.listId;
+  const statement = 'SELECT id, name FROM todolist WHERE user_id = ? AND id = ?';
 
-  const sql = 'select id, name from todolist where user_id = ? and id = ?';
-
-  connection.query(sql, [userId, listId], (err, results) => {
-    if (err) {
-      res.status(404).json({ error: err });
-    } else {
-      res.status(200).json({ userId: userId, listId: listId, results });
-    }
-  });
+  const results = await getData(res, statement, [userId, listId]);
+  res.status(200).json({ userId: userId, listId: listId, results });
 });
 
 // Create a todo list
-app.post(`/:userId/lists`, (req, res) => {
+app.post('/:userId/lists', async (req, res) => {
   const userId = req.params.userId;
+  const statement = `INSERT INTO todolist (name, user_id) VALUES ('Newly created list to todo', ?)`;
 
-  const sql = `insert into todolist (name, user_id) values('Newly created list to todo', ?)`;
-
-  connection.query(sql, [userId], (err, results) => {
-    if (err) {
-      res.status(404).json({ error: err });
-    } else {
-      res.status(200).json({ userId: userId, listId: listId, results });
-    }
-  });
+  const results = await getData(res, statement, [userId]);
+  const listId = results.insertId;
+  res.status(200).json({ userId: userId, listId: listId, results });
 });
 
 // Delete a todo list by id
-app.delete(`/:userId/lists/:listId`, (req, res) => {
+app.delete('/:userId/lists/:listId', async (req, res) => {
   const userId = req.params.userId;
   const listId = req.params.listId;
+  const statement = `DELETE FROM todolist WHERE user_id = ? AND id = ?`;
 
-  const sql = `delete from todolist where user_id = ? and id = ?`;
-
-  connection.query(sql, [userId, listId], (err, results) => {
-    if (err) {
-      res.status(404).json({ error: err });
-    } else {
-      res.status(200).json({
-        userId: userId,
-        listId: listId,
-        results: `List with id ${listId} is deleted successfully`,
-      });
-    }
+  await getData(res, statement, [userId, listId]);
+  res.status(200).json({
+    userId: userId,
+    listId: listId,
+    results: `List with id ${listId} is deleted successfully`,
   });
 });
 
 // Add reminder to the list
-app.post(`/:userId/lists/:listId/reminders`, (req, res) => {
+app.post('/:userId/lists/:listId/reminders', async (req, res) => {
   const listId = req.params.listId;
+  const statement = `INSERT INTO listreminders (remind_at, list_id) VALUES ('2024-11-12 22:10:00', ?)`;
 
-  const sql = `insert into listreminders (remind_at, list_id) values('2024-11-12 22:10:00', ?)`;
-
-  connection.query(sql, [listId], (err, results) => {
-    if (err) {
-      res.status(404).json({ error: err });
-    } else {
-      res.status(200).json({
-        listId: listId,
-        results: `Reminder for list ${listId} is added successfully`,
-      });
-    }
+  await getData(res, statement, [listId]);
+  res.status(200).json({
+    listId: listId,
+    results: `Reminder for list ${listId} is added successfully`,
   });
 });
 
 // ---- ITEMS ----
 // Insert item(s) in todo list
-app.post(`/:userId/lists/:listId/items`, (req, res) => {
+app.post('/:userId/lists/:listId/items', async (req, res) => {
   const userId = req.params.userId;
   const listId = req.params.listId;
+  const statement = `INSERT INTO todoitem (name, list_id, completed) VALUES ('Buy something new', ?, 0)`;
 
-  const sql = `insert into todoitem (name, list_id, completed) values('Buy something new', ?, 0)`;
-
-  connection.query(sql, [listId], (err, results) => {
-    if (err) {
-      res.status(404).json({ error: err });
-    } else {
-      res.status(200).json({
-        listId: listId,
-        results: `Todoitem into list ${listId} is added successfully`,
-      });
-    }
+  await getData(res, statement, [listId]);
+  res.status(200).json({
+    listId: listId,
+    results: `Todoitem into list ${listId} is added successfully`,
   });
 });
 
 // Delete item(s) from todo list
-app.delete(`/:userId/lists/:listId/items/:itemId`, (req, res) => {
+app.delete('/:userId/lists/:listId/items/:itemId', async (req, res) => {
   const itemId = req.params.itemId;
   const listId = req.params.listId;
+  const statement = `DELETE FROM todoitem WHERE id = ? AND list_id = ?`;
 
-  const sql = `delete from todoitem where id = ? and list_id = ?`;
-
-  connection.query(sql, [itemId, listId], (err, results) => {
-    if (err) {
-      res.status(404).json({ error: err });
-    } else {
-      res.status(200).json({
-        listId: listId,
-        results: `Item with id ${itemId} is deleted successfully`,
-      });
-    }
+  await getData(res, statement, [itemId, listId]);
+  res.status(200).json({
+    listId: listId,
+    results: `Item with id ${itemId} is deleted successfully`,
   });
 });
 
 // Mark item as completed
-app.patch(`/:userId/lists/:listId/items/:itemId`, (req, res) => {
+app.patch('/:userId/lists/:listId/items/:itemId', async (req, res) => {
   const listId = req.params.listId;
   const itemId = req.params.itemId;
+  const statement = `UPDATE todoitem SET completed = true WHERE id = ? AND list_id = ?`;
 
-  const sql = `update todoitem set completed = true where id = ? and list_id = ?`;
-  connection.query(sql, [itemId, listId], (err, results) => {
-    if (err) {
-      res.status(404).json({ error: err });
-    } else {
-      res.status(200).json({
-        listId: listId,
-        results: `Item with id ${itemId} is marked completed`,
-      });
-    }
+  await getData(res, statement, [itemId, listId]);
+  res.status(200).json({
+    listId: listId,
+    results: `Item with id ${itemId} is marked completed`,
   });
 });
 

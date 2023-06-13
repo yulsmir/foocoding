7a49aaff237c28340e8d663939b753eb6f75627e
Yuliia Smirnova <julianna.smirnova@gmail.com>
2023-06-13 15:17:24 +0200

    Add tast decription to readme, add planned function to handlers

---
+++ Node.js/Week1/node-http-server/src/domains/posts/postsHandler.js
@@ -0,0 +1,4 @@
+// GET -- Get all posts
+// GET -- Get one post by id
+// PATCH -- Edit post
+// DELETE -- Delete post
---
+++ Node.js/Week1/node-http-server/src/domains/users/usersHandler.js
@@ -0,0 +1,4 @@
+// GET -- Get all users
+// GET -- Get one user by id
+// PATCH -- Edit user
+// DELETE -- Delete user

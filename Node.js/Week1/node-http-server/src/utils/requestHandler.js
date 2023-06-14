d950d1cb372a3591d6c9465386dbb58752b9f55e
Yuliia Smirnova <julianna.smirnova@gmail.com>
2023-06-14 14:30:37 +0200

    Add CRUD for users

--- Node.js/Week1/node-http-server/src/utils/requestHandler.js
+++ Node.js/Week1/node-http-server/src/utils/requestHandler.js
@@ -1,6 +1,7 @@
 import { ReasonPhrases, StatusCodes } from 'http-status-codes';
 
 import { getRequestData } from './getRequestData.js';
+import { readJsonFile, writeJsonFile } from './fileHandler.js';
 import { IncomingMessage, ServerResponse } from 'http';
 
 /**
@@ -24,10 +25,71 @@ export const requestHandler = async (request, response) => {
 
   switch (path) {
     case 'users': {
+      const body = await getRequestData(request);
+
       const usersPattern = new URLPattern({ pathname: '/users/:id' });
       const usersEndpoint = usersPattern.exec(fullEndpoint);
-      const id = usersEndpoint?.pathname?.groups?.id;
+
+      const idIdentifier = '/users/';
+      const idIndex = url.indexOf(idIdentifier);
+      const idStartIndex = idIndex + idIdentifier.length;
+      const id = parseInt(url.slice(idStartIndex));
+
+      switch (method) {
+        case 'POST':
+          response.statusCode = StatusCodes.CREATED;
+          data.error = ReasonPhrases.CREATED;
+          data.message = 'New user added';
+          break;
+
+        case 'GET':
+          if (id) {
+            response.statusCode = StatusCodes.OK;
+            data.error = ReasonPhrases.OK;
+            data.message = `Getting user by id ${id}`;
+          } else {
+            data.error = ReasonPhrases.OK;
+            data.message = `Getting ALL Users`;
+          }
+          break;
+
+        case 'PATCH':
+          if (id) {
+            response.statusCode = StatusCodes.CREATED;
+            data.error = ReasonPhrases.CREATED;
+            data.message = `Editing user by id ${id}`;
+          } else {
+            data.error = ReasonPhrases.NOT_FOUND;
+            response.statusCode = StatusCodes.NOT_FOUND;
+            data.message = `No user id is specified`;
+          }
+          break;
+
+        case 'DELETE':
+          if (id) {
+            response.statusCode = StatusCodes.ACCEPTED;
+            data.error = ReasonPhrases.ACCEPTED;
+            data.message = `Deleted user by id ${id}`;
+          } else {
+            data.error = ReasonPhrases.response.statusCode = StatusCodes.BAD_REQUEST;
+            data.message = `No user id is specified`;
+          }
+          break;
+
+        default:
+          break;
+      }
+
+      break;
+    }
+
+    case 'posts': {
       const body = await getRequestData(request);
+      const postsPattern = new URLPattern({ pathname: '/posts/:id' });
+      const postsEndpoint = postsPattern.exec(fullEndpoint);
+      const id = postsEndpoint?.pathname?.groups?.id;
+
+      console.log(`dealing with posts - id: ${postsEndpoint.pathname.groups.id}`);
 
       switch (method) {
         case 'POST':
@@ -66,18 +128,6 @@ export const requestHandler = async (request, response) => {
         default:
           break;
       }
-
-      break;
-    }
-
-    case 'posts': {
-      const body = await getRequestData(request);
-      const postsPattern = new URLPattern({ pathname: '/posts/:id' });
-      const postsEndpoint = postsPattern.exec(fullEndpoint);
-      const id = postsEndpoint?.pathname?.groups?.id;
-
-      console.log(`dealing with posts - id: ${postsEndpoint.pathname.groups.id}`);
-
       break;
     }
   }

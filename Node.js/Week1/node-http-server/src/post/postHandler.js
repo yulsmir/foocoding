1d1c557367ecd4907da87739bb7fbd92ae4babf9
Yuliia Smirnova <julianna.smirnova@gmail.com>
2023-06-16 16:57:54 +0200

    Move code to postHandler and userHandler

---
+++ Node.js/Week1/node-http-server/src/post/postHandler.js
@@ -0,0 +1,33 @@
+'use strict';
+
+import { readJsonFile, writeJsonFile } from '../utils/fileHandler.js';
+
+export const getPosts = async () => {
+  const postsData = await readJsonFile('./data/posts.json');
+  return JSON.parse(postsData);
+};
+
+export const getPostById = async (id) => {
+  const postsData = await readJsonFile('./data/posts.json');
+  const posts = JSON.parse(postsData);
+  return posts.find((post) => post.post_id === parseInt(id));
+};
+
+export const addPost = async (post) => {
+  const posts = await getPosts();
+  posts.push(post);
+  await writeJsonFile('./data/posts.json', posts);
+};
+
+export const updatePost = async (updatedPost) => {
+  const posts = await getPosts();
+  const index = posts.findIndex((post) => post.post_id === updatedPost.post_id);
+  posts[index] = updatedPost;
+  await writeJsonFile('./data/posts.json', posts);
+};
+
+export const deletePost = async (id) => {
+  const posts = await getPosts();
+  const filteredPosts = posts.filter((post) => post.post_id !== parseInt(id));
+  await writeJsonFile('./data/posts.json', filteredPosts);
+};
---
+++ Node.js/Week1/node-http-server/src/user/userHandler.js
@@ -0,0 +1,33 @@
+'use strict';
+
+import { readJsonFile, writeJsonFile } from '../utils/fileHandler.js';
+
+export const getUsers = async () => {
+  const usersData = await readJsonFile('./data/users.json');
+  return JSON.parse(usersData);
+};
+
+export const getUserById = async (id) => {
+  const usersData = await readJsonFile('./data/users.json');
+  const users = JSON.parse(usersData);
+  return users.find((user) => user.id === parseInt(id));
+};
+
+export const addUser = async (user) => {
+  const users = await getUsers();
+  users.push(user);
+  await writeJsonFile('./data/users.json', users);
+};
+
+export const updateUser = async (updatedUser) => {
+  const users = await getUsers();
+  const index = users.findIndex((user) => user.id === updatedUser.id);
+  users[index] = updatedUser;
+  await writeJsonFile('./data/users.json', users);
+};
+
+export const deleteUser = async (id) => {
+  const users = await getUsers();
+  const filteredUsers = users.filter((user) => user.id !== parseInt(id));
+  await writeJsonFile('./data/users.json', filteredUsers);
+};

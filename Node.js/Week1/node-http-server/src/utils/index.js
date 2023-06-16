6680d17c46e8ee63169c5b814c038caa2d95f87d
Yuliia Smirnova <julianna.smirnova@gmail.com>
2023-06-16 17:01:08 +0200

    Add exports to index.js

--- Node.js/Week1/node-http-server/src/utils/index.js
+++ Node.js/Week1/node-http-server/src/utils/index.js
@@ -1,3 +1,5 @@
 export * from './getRequestData.js';
 export * from './requestHandler.js';
 export * from './fileHandler.js';
+export * from '../post/postHandler.js';
+export * from '../user/userHandler.js';
